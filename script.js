c2881742d535b753bba39351edad1fe9db319041
Jabez Villamarzo <jabezvargas.villamarzo@bicol-u.edu.ph>
2023-11-24 23:29:58 +0800

    Add input empty checks

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const form = document.querySelector(".main-container form");
+	const inputs = document.querySelectorAll("form input");
 
-	form.addEventListener("submit", (event) => {
-		event.preventDefault();		
-		alert("submitted");
-	});
+	form.addEventListener("submit", submit_form_handler);
+	
+	for (const input of inputs) {
+		input.addEventListener("input", (event) => {
+			input_check_if_empty(event.target);
+		});	
+	}
 });
+
+function input_check_if_empty(input) {
+	if (input.value === "") {
+		input.setCustomValidity("Must not be empty.");
+		return true;
+	}
+
+	input.setCustomValidity("");
+	input.reportValidity();
+	return false;
+}
+
+function submit_form_handler(event) {
+	event.preventDefault();
+	
+	const form = event.target;
+	const inputs = form.querySelectorAll("li input");
+
+	for (const input of inputs) {
+		if (input_check_if_empty(input)) {
+			input.reportValidity();
+			break;
+		}
+	}
+}
