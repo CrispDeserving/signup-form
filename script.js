bafff4b6775f05d0519f486b2491cf048e99c801
Jabez Villamarzo <simplesimmering@gmail.com>
2023-11-26 11:34:37 +0800

    Tweak input validation UX Add input validation for phone-number

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const form = document.querySelector(".main-container form");
 	const inputs = document.querySelectorAll("form input");
+	const password = form.querySelector("#password");
+	const password_confirm = form.querySelector("#password-confirm");
 
 	form.addEventListener("submit", submit_form_handler);
 	
 	for (const input of inputs) {
 		input.addEventListener("input", (event) => {
 			input_check_if_empty(event.target);
-		});	
+		});
+	}
+
+	for (const password_input of [password, password_confirm]) {
+		password_input.addEventListener("input", () => {
+			verify_passwords_match(password, password_confirm);
+		});
 	}
 });
+
+function verify_passwords_match(password, password_confirm) {
+	if (password.value !== password_confirm.value) {
+		password.setCustomValidity("Passwords must match.");
+		password_confirm.setCustomValidity("Passwords must match.");
+		return false;
+	}
+
+	password.setCustomValidity("");
+	password_confirm.setCustomValidity("");
+	return true;
+}
 
 function input_check_if_empty(input) {
 	if (input.value === "") {
@@ -18,7 +38,6 @@ function input_check_if_empty(input) {
 	}
 
 	input.setCustomValidity("");
-	input.reportValidity();
 	return false;
 }
 
@@ -33,8 +52,10 @@ function submit_form_handler(event) {
 		if (input_check_if_empty(input)) {
 			input_invalid = true;
 		}
-
-		if (input.validity.typeMismatch) {
+		
+		if (!input_invalid
+			&& (input.validity.typeMismatch || input.validity.patternMismatch)
+		){
 			let type_string = "";
 			switch (input.type) {
 				case "email":
@@ -50,7 +71,6 @@ function submit_form_handler(event) {
 				break;
 			}
 
-			console.log("here");
 			input.setCustomValidity(`Input does not look like ${type_string}.`);
 			input_invalid = true;
 		}
@@ -60,18 +80,14 @@ function submit_form_handler(event) {
 			return;
 		}
 	}
-
+	
 	const password = form.querySelector("#password");
 	const password_confirm = form.querySelector("#password-confirm");
-
-	if (password.value !== password_confirm.value) {
-		password.setCustomValidity("Passwords must match.");
-		password_confirm.setCustomValidity("Passwords must match.");
-
+	if (!verify_passwords_match(password, password_confirm)) {
 		password.reportValidity();
 		return;
 	}
 
-	form.submit();
+	// form.submit();
 	alert("Form submitted!");
 }
